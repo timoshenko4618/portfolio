aca9fbe7d28f9ba0388f1f47837d0a9e6b3e27b9
Eugene Timoshenko <timoshenko4618@yandex.ru>
2020-08-21 12:15:26 +0300

    перенес все проекты в этот репозиторий

---
+++ header-34/gulpfile.js/tasks/buildLayouts.js
@@ -0,0 +1,8 @@
+const gulp = require("gulp");
+const paths = require("../paths.js");
+
+module.exports = function buildLayouts(cb) {
+  return gulp
+  .src(paths.layouts.srcFiles)
+  .pipe(gulp.dest(paths.distFolder));
+};
