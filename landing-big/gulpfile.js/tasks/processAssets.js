a903267e0790ea376280b52bc08ddcfc624357ba
Eugene Timoshenko <timoshenko4618@yandex.ru>
2020-09-14 21:12:25 +0300

    landing-big: каркас

---
+++ landing-big/gulpfile.js/tasks/processAssets.js
@@ -0,0 +1,8 @@
+const gulp = require("gulp");
+const paths = require("../paths.js");
+
+module.exports = function processAssets() {
+  return gulp
+  .src(paths.assets.srcFiles)
+  .pipe(gulp.dest(paths.assets.distFolder));
+};
