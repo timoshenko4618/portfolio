1048d8f677591e0005fe70ab580546cbbbd1d47d
Eugene Timoshenko <timoshenko4618@yandex.ru>
2020-09-16 14:07:08 +0300

    OMSver2: prettier

--- online medical services ver2/gulpfile.js/tasks/buildStyles.js
+++ online medical services ver2/gulpfile.js/tasks/buildStyles.js
@@ -7,20 +7,23 @@ const gulpSourcemaps = require("gulp-sourcemaps");
 const paths = require("../paths.js");
 
 module.exports = function buildStyles() {
-  return gulp.src(paths.styles.srcMainFile)
-    .pipe(gulpSourcemaps.init())
-    .pipe(
-      gulpSass({
-        outputStyle: "compressed"
-      }).on("error", gulpSass.logError)
-    )
-    .pipe(gulpRename({ basename: "styles", suffix: ".min" }))
-    //.pipe(gulpAutoprefixer(["last 15 versions"]))
-    .pipe(
-      gulpSourcemaps.write({
-        includeContent: false,
-        sourceRoot: `../${paths.styles.relativeSrcFolder}`
-      })
-    )
-    .pipe(gulp.dest(paths.distFolder));
+  return (
+    gulp
+      .src(paths.styles.srcMainFile)
+      .pipe(gulpSourcemaps.init())
+      .pipe(
+        gulpSass({
+          outputStyle: "compressed"
+        }).on("error", gulpSass.logError)
+      )
+      .pipe(gulpRename({ basename: "styles", suffix: ".min" }))
+      //.pipe(gulpAutoprefixer(["last 15 versions"]))
+      .pipe(
+        gulpSourcemaps.write({
+          includeContent: false,
+          sourceRoot: `../${paths.styles.relativeSrcFolder}`
+        })
+      )
+      .pipe(gulp.dest(paths.distFolder))
+  );
 };
--- online medical services ver2/gulpfile.js/tasks/processAssets.js
+++ online medical services ver2/gulpfile.js/tasks/processAssets.js
@@ -2,7 +2,5 @@ const gulp = require("gulp");
 const paths = require("../paths.js");
 
 module.exports = function processAssets() {
-  return gulp
-  .src(paths.assets.srcFiles)
-  .pipe(gulp.dest(paths.assets.distFolder));
+  return gulp.src(paths.assets.srcFiles).pipe(gulp.dest(paths.assets.distFolder));
 };
